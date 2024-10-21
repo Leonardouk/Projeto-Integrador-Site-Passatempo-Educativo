473df996416ba7e3cbeebb6085d28255ceab6c60
Leonardo F B <leonardofb31@gmail.com>
2024-10-21 19:22:57 -0300

    Integração de imagens com DB, não definitivo

--- prototipos/front/js/script.js
+++ prototipos/front/js/script.js
@@ -4,16 +4,22 @@ const testeEndpoint = '/teste'
 let modoAdmin = false
 
 async function obterTexto() {
-    //Define o arrayTodosTextos por meio da comunicação com o DB
+    //Define o arrayTodosTextos e arrayTodasImagens por meio da comunicação com o DB
     const URLCompleta = `${protocolo}${baseURL}${testeEndpoint}`
-    const arrayTodosTextos = ((await axios.get(URLCompleta)).data)
+    const arrayElementos = ((await axios.get(URLCompleta)).data)
+    const arrayTodosTextos = arrayElementos[0]
+    const arrayTodasImagens = arrayElementos[1]
     
     arrayTodosTextos.forEach(element => {
         if (element.pagina == "teste") {
-            // console.log('foi')
-            // arrayTextosPagina.push({texto: element.texto, ordem: element.ordem})
             let paragrafo = document.querySelector(`#texto${element.ordem}`)
             paragrafo.innerHTML = element.texto
+        }
+    })
+    arrayTodasImagens.forEach(element => {
+        if (element.pagina == "teste") {
+            let imagem = document.querySelector(`#img${element.ordem}`)
+            imagem.src = element.linkImagem
         }
     })
 }
--- prototipos/index.js
+++ prototipos/index.js
@@ -15,6 +15,12 @@ const Texto = mongoose.model("Texto", mongoose.Schema({
     texto: {type: String}
 }))
 
+const Imagem = mongoose.model("Imagen", mongoose.Schema({
+    ordem: {type: Number},
+    pagina: {type: String},
+    linkImagem: {type: String}
+}))
+
 async function conectarAoMongoDB() {
     await
     mongoose.connect('mongodb+srv://Leonardo:SENHAPI@cluster0.xt0db.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
@@ -22,12 +28,16 @@ async function conectarAoMongoDB() {
 
 app.get("/teste", async (req, res) => {
     const textos = await Texto.find()
-    res.json(textos)
+    const imagens = await Imagem.find()
+    let arrayElementos = [textos, imagens]
+    res.json(arrayElementos)
 })
 
 app.post("/teste", async (req, res) => {
     const textos = await Texto.find()
-    res.json(textos)
+    const imagens = await Imagem.find()
+    let arrayElementos = [textos, imagens]
+    res.json(arrayElementos)
 })
 
 app.listen(3000, () => {
