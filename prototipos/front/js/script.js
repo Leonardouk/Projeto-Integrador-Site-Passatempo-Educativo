a39b8196941a032eb4eff6839e1d5cf63f341077
Leonardo Formagio Bicudo <leonardofb31@gmail.com>
2024-10-21 17:36:42 -0300

    Implementação do DB para busca de elementos de texto.

--- prototipos/front/js/script.js
+++ prototipos/front/js/script.js
@@ -1,6 +1,7 @@
 const protocolo = 'http://'
 const baseURL = 'localhost:3000'
 const testeEndpoint = '/teste'
+let modoAdmin = false
 
 async function obterTexto() {
     //Define o arrayTodosTextos por meio da comunicação com o DB
@@ -17,7 +18,6 @@ async function obterTexto() {
     })
 }
 
-let modoAdmin = false
 async function trocarModoAdmin() {
     //Define o arrayTodosTextos por meio da comunicação com o DB
     const URLCompleta = `${protocolo}${baseURL}${testeEndpoint}`
@@ -27,39 +27,30 @@ async function trocarModoAdmin() {
     modoAdmin ? modoAdmin = false : modoAdmin = true;
     console.log(`o modo admin está ligado: ${modoAdmin}`)
 
+    //Caso o modo admin esteja ativo, todos os elementos são buscados no array e colocados em seus respectivos espaços no arquivo html e os torna editáveis
     if (modoAdmin == true) {
         arrayTodosTextos.forEach(element => {
             if (element.pagina == "teste") {
-                let paragrafo = (document.querySelector(`#texto${element.ordem}`)).value
-                console.log(paragrafo)
+                let paragrafo = document.querySelector(`#texto${element.ordem}`)
                 let texto = paragrafo.innerHTML
-                paragrafo.outerHTML = "<textarea class=\"form-control\" name=\"test\" id=\"textoComum\" style=\"height: 300px;\"></textarea>"
+                paragrafo.outerHTML = `<textarea class=\"form-control\" name=\"test\" id=\"texto${element.ordem}\" style=\"height: 300px;\"></textarea>`
                 paragrafo = document.querySelector(`#texto${element.ordem}`)
                 paragrafo.innerHTML = texto
             }
         });
-        //Lê o elemento pelo id e o seu valor interno, muda o elemento de <p> para <textarea> e preserva o valor interno
-        //Na próxima versão do arquivo, ao invés de preservar o valor apenas na variável, ele precisará encontrar o valor no DB
-        // let paragrafo = document.getElementById('textoComum')
-        // let texto = paragrafo.innerHTML
-        // paragrafo.outerHTML = "<textarea class=\"form-control\" name=\"test\" id=\"textoComum\" style=\"height: 300px;\"></textarea>"
-        // paragrafo = document.querySelector('#textoComum')
-        // paragrafo.innerHTML = texto
     }
+    //Caso o modo admin seja desativado, todos os elementos são buscados no array e colocados em seus respectivos espaços no arquivo html, salvando seu conteúdo no DB
     else {
         arrayTodosTextos.forEach(element => {
-            let paragrafo = document.querySelector(`#texto${element.ordem}`)
-            let texto = paragrafo.innerHTML
-            paragrafo.outerHTML = "<textarea class=\"form-control\" name=\"test\" id=\"textoComum\" style=\"height: 300px;\"></textarea>"
-            paragrafo = document.querySelector('#textoComum')
-            paragrafo.innerHTML = texto
+            if (element.pagina == "teste") {
+                let paragrafo = document.querySelector(`#texto${element.ordem}`)
+                let texto = paragrafo.value
+                console.log(paragrafo)
+                paragrafo.outerHTML = `<p style=\"text-indent: 1em;\" id=\"texto${element.ordem}\"></p>`
+                paragrafo = document.querySelector(`#texto${element.ordem}`)
+                console.log(texto)
+                paragrafo.innerHTML = texto
+            }
         });
-        //Lê o elemento pelo id e o seu valor interno, muda o elemento de <textarea> para <p> e preserva o valor interno
-        //Na próxima versão do arquivo, ao invés de preservar o valor apenas na variável, ele precisará encontrar e salvar o valor no DB
-        let paragrafo = document.getElementById('textoComum')
-        let texto = paragrafo.value
-        paragrafo.outerHTML = "<p style=\"text-indent: 1em;\" id=\"textoComum\"></p>"
-        paragrafo = document.querySelector('#textoComum')
-        paragrafo.innerHTML = texto
     }
 }
