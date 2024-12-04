21b85ac4795c27548ce32bfe1d1b73a994197f90
Leonardo Formagio Bicudo <leonardofb31@gmail.com>
2024-12-04 17:01:32 -0300

    Adição de alteração de Navbar

--- js/estatistica.js
+++ js/estatistica.js
@@ -6,7 +6,24 @@ function conectarEndpoint(endpoint) {
     return URLCompleta
 }
 
-async function obterDados() {
+function obterDados() {
+    const URLCompleta = conectarEndpoint('/log-sessao')
     let tabela = document.querySelector('.dados')
-    let corpoTabela
+    let corpoTabela = document.getElementsByTagName('tbody')[0]
+    corpoTabela.innerHTML = ""
+
+    for (let dado of dados) {
+        let linha = corpoTabela.insertRow(0)
+        let celulaID = linha.insertCell(0)
+        let celulaIP = linha.insertCell(1)
+        let celulaEntrada = linha.insertCell(2)
+        let celulaSaida = linha.insertCell(3)
+        let celulaDuracao = linha.insertCell(4)
+
+        celulaID.innerHTML = dado.sessionId
+        celulaIP.innerHTML = dado.ipAddress
+        celulaEntrada.innerHTML = dado.startTime
+        celulaSaida.innerHTML = dado.endTime
+        celulaDuracao = dado.sessionDuration
+    }
 }
